444fe625b7d33b3df97e111a1362707630382a4a
장수홍 <shjang@miridih.com>
2021-08-26 14:49:59 +0900

    #6 장비 상점/인벤토리 나눠야 할 듯

--- app/src/main/java/com/innocent/growingdeveloperclickergame/equip/Equip.kt
+++ app/src/main/java/com/innocent/growingdeveloperclickergame/equip/Equip.kt
@@ -1,11 +1,15 @@
 package com.innocent.growingdeveloperclickergame.equip
 
-class Equip(price: Int, codingPowerRate: Int) {
+class Equip(type: EquipType, price: Int, codingPowerRate: Int, resourceId: String) {
+    val type: EquipType
     val price: Int
     val codingPowerRate: Int
+    val resourceId: String
 
     init {
+        this.type = type
         this.price = price
         this.codingPowerRate = codingPowerRate
+        this.resourceId = resourceId
     }
 }
--- app/src/main/java/com/innocent/growingdeveloperclickergame/equip/EquipDC.kt
+++ app/src/main/java/com/innocent/growingdeveloperclickergame/equip/EquipDC.kt
@@ -5,20 +5,24 @@ import com.innocent.growingdeveloperclickergame.main.CodingPowerDC
 import com.innocent.growingdeveloperclickergame.main.MoneyDC
 
 object EquipDC {
-    private val equips: Array<Equip> = arrayOf(Equip(2000, 9))
+    private val equipsShop: Array<Equip> = arrayOf(
+        Equip(EquipType.CHAIR, 1000, 3, "@drawable/chair2"),
+        Equip(EquipType.TABLE, 1000, 3, "@drawable/desk2"),
+        Equip(EquipType.MONITER, 1000, 3, "@drawable/monitor2")
+    )
 
     fun buyEquip(equipIdx: Int) {
         Log.d("EquipDC", "buyEquip")
         if (!canBuyEquip(equipIdx)) return
 
-        val equip = equips[equipIdx]
+        val equip = equipsShop[equipIdx]
         CodingPowerDC.addCodingPowerRate(equip.codingPowerRate)
         MoneyDC.minusMoney(equip.price)
     }
 
     fun canBuyEquip(equipIdx: Int): Boolean {
         Log.d("EquipDC", "canBuyEquip")
-        val isValidIdx: Boolean = equipIdx >= 0 && equipIdx < equips.size
-        return isValidIdx && equips[equipIdx].price <= MoneyDC.getMoney()
+        val isValidIdx: Boolean = equipIdx >= 0 && equipIdx < equipsShop.size
+        return isValidIdx && equipsShop[equipIdx].price <= MoneyDC.getMoney()
     }
 }
