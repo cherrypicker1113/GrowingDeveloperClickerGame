6b80647a788fb58f791b6797fdf2cdd52cf2f700
cherrypicker1113 <cherrypicker1113@gmail.com>
2021-10-07 20:50:37 +0900

    광고 넣기 테스트

--- app/src/main/java/com/innocent/growingdeveloperclickergame/common/ToastController.kt
+++ app/src/main/java/com/innocent/growingdeveloperclickergame/common/ToastController.kt
@@ -1,17 +1,17 @@
 package com.innocent.growingdeveloperclickergame.common
 
-import android.app.Activity
+import android.content.Context
 import android.view.View
 import android.widget.Toast
 
 object ToastController {
     private var currentToast: Toast? = null
 
-    fun showToast(activity: Activity, text: String) {
+    fun showToast(context: Context, text: String) {
         if (currentToast?.view?.windowVisibility == View.VISIBLE) {
             currentToast!!.cancel()
         }
-        currentToast = Toast.makeText(activity, text, Toast.LENGTH_SHORT)
+        currentToast = Toast.makeText(context, text, Toast.LENGTH_SHORT)
         currentToast!!.show()
     }
 }
